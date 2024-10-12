5535dba152394c391fbab1e192e0aaa2faba8dfc
Valery <karev.valera2013@yandex.ru>
2024-10-12 21:30:01 +0300

    Работы по спринту №11

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,71 +1,161 @@
 package com.practicum.playlistmaker
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
-import android.text.InputType
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.getSystemService
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlin.random.Random
+import com.practicum.playlistmaker.api.TrackSearch
+import com.practicum.playlistmaker.data.TrackResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
-    private val trackList = mutableListOf<Track>()
-
-    init {
-        (1..50).map {
-            val defaultTrack = DEFAULT_TRACK_LIST[Random.nextInt(0, DEFAULT_TRACK_LIST.size)]
-            trackList.add(
-                Track(
-                    defaultTrack.trackName,
-                    defaultTrack.singerName,
-                    defaultTrack.duration,
-                    defaultTrack.src
-                )
-            )
-        }
+    private enum class ErrorType {
+        NOT_FOUND, CONNECTIONS_PROBLEMS
     }
+
+    private val trackListAdapter = TrackListAdapter(mutableListOf())
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val searchTrackApi = retrofit.create(TrackSearch::class.java)
+
+    private lateinit var buttonBack: Button
+    private lateinit var resetButton: ImageView
+    private lateinit var searchInput: EditText
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var errorContainer: LinearLayout
+    private lateinit var errorText: TextView
+    private lateinit var updateRequestButton: Button
+    private lateinit var errorImage: ImageView
 
     private var searchInputValue: String = DEFAULT_INPUT_VALUE
 
     private fun initRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.track_list)
-        recyclerView.adapter = TrackListAdapter(trackList.toList())
+        recyclerView = findViewById(R.id.track_list)
+        recyclerView.adapter = trackListAdapter
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+
+    private fun showErrorContainer(errorType: ErrorType) {
+        if (trackListAdapter.trackList.isNotEmpty()) {
+            trackListAdapter.trackList.clear()
+            trackListAdapter.notifyDataSetChanged()
+        }
+        errorContainer.visibility = View.VISIBLE
+        errorImage.visibility = View.VISIBLE
+        errorText.visibility = View.VISIBLE
+        when (errorType) {
+            ErrorType.NOT_FOUND -> {
+                errorImage.setImageResource(R.drawable.nothing_found)
+                errorText.text = getString(R.string.search_error_nothing_found)
+                updateRequestButton.visibility = View.GONE
+            }
+            ErrorType.CONNECTIONS_PROBLEMS -> {
+                errorImage.setImageResource(R.drawable.connection_problems)
+                errorText.text = getString(R.string.search_error_internet)
+                updateRequestButton.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun hideErrorContainer() {
+        errorContainer.visibility = View.GONE
+        errorText.visibility = View.GONE
+        updateRequestButton.visibility = View.GONE
+        errorImage.visibility = View.GONE
+    }
+
+    private fun makeRequest(searchText: String) {
+        searchTrackApi.getTrackList(searchText).enqueue(object : Callback<TrackResponse> {
+            override fun onResponse(call: Call<TrackResponse>, response: Response<TrackResponse>) {
+                if (response.code() == 200) {
+                    if (response.body()?.resultCount!! > 0) {
+                        trackListAdapter.trackList.clear()
+                        trackListAdapter.trackList.addAll(response.body()?.results!!)
+                        hideErrorContainer()
+                        trackListAdapter.notifyDataSetChanged()
+                    } else {
+                        showErrorContainer(ErrorType.NOT_FOUND)
+                    }
+                } else {
+                    showErrorContainer(ErrorType.NOT_FOUND)
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                showErrorContainer(ErrorType.CONNECTIONS_PROBLEMS)
+            }
+        })
+    }
+
+    private fun handleSearchInput() {
+        searchInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (searchInput.text.isNotEmpty()) {
+                    makeRequest(searchInput.text.toString())
+                } else {
+                    trackListAdapter.trackList.clear()
+                    trackListAdapter.notifyDataSetChanged()
+                }
+                true
+            }
+            false
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val buttonBack = findViewById<Button>(R.id.button_back)
+        buttonBack = findViewById(R.id.button_back)
+        resetButton = findViewById(R.id.reset_button)
+        searchInput = findViewById(R.id.search)
+        errorContainer = findViewById(R.id.errorContainer)
+        errorText = findViewById(R.id.errorText)
+        updateRequestButton = findViewById(R.id.updateRequest)
+        errorImage = findViewById(R.id.errorImage)
+
         buttonBack.setOnClickListener {
             finish()
         }
 
-        val resetButton = findViewById<ImageView>(R.id.reset_button)
-        val searchInput = findViewById<EditText>(R.id.search)
+        updateRequestButton.setOnClickListener {
+            makeRequest(searchInput.text.toString())
+        }
+
         if (searchInputValue.isNotEmpty()) {
             searchInput.setText(searchInputValue)
         }
+
         resetButton.setOnClickListener {
             searchInput.setText(DEFAULT_INPUT_VALUE)
-
             val inputMethodManager = getSystemService<InputMethodManager>()
             inputMethodManager?.hideSoftInputFromWindow(resetButton.windowToken, 0)
-            searchInput.clearFocus()
+            if (trackListAdapter.trackList.isNotEmpty()) {
+                trackListAdapter.trackList.clear()
+                trackListAdapter.notifyDataSetChanged()
+                searchInput.clearFocus()
+            }
+            //Если нажимаем "X", в случае когда ничего не найдено, то скрываем контейнер с ошибкой
+            hideErrorContainer()
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -94,7 +184,10 @@ class SearchActivity : AppCompatActivity() {
         searchInput.addTextChangedListener(simpleTextWatcher)
 
         initRecyclerView()
+        handleSearchInput()
     }
+
+
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -109,44 +202,6 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         const val INPUT_VALUE = "INPUT_VALUE"
         const val DEFAULT_INPUT_VALUE = ""
-
-        private val DEFAULT_TRACK_LIST = listOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Sweet Child O'Mine. Loooong string. Loooong string. Loooong string.",
-                "Guns N' Roses. Guns N' Roses. Guns N' Roses. Guns N' Roses.",
-                "5:035:035:035:035:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
+        const val BASE_URL = "https://itunes.apple.com/"
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/TrackListAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackListAdapter.kt
@@ -1,59 +1,60 @@
 package com.practicum.playlistmaker
 
 import android.content.Context
-import android.util.Log
 import android.util.TypedValue
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.data.Track
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TrackListAdapter(
-    private val trackList: List<Track>
-): RecyclerView.Adapter<TrackListAdapter.TrackViewHolder>() {
+    val trackList: MutableList<Track>
+) : RecyclerView.Adapter<TrackListAdapter.TrackViewHolder>() {
 
     override fun getItemCount(): Int = trackList.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        return TrackViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_track, parent, false))
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder = TrackViewHolder(parent)
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        Log.d("SPRINT_10", holder.toString())
         holder.bind(trackList[position])
     }
 
-    class TrackViewHolder(parentView: View): RecyclerView.ViewHolder(parentView) {
-        private val albumPictureView: ImageView = parentView.findViewById(R.id.album_picture)
-        private val trackView: TextView = parentView.findViewById(R.id.track_name)
-        private val musicalView: TextView = parentView.findViewById(R.id.musical_artist_name)
-        private val directionView: TextView = parentView.findViewById(R.id.duration)
+    class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_track, parent, false)
+    ) {
+        private val albumPictureView: ImageView = itemView.findViewById(R.id.album_picture)
+        private val trackView: TextView = itemView.findViewById(R.id.track_name)
+        private val musicalView: TextView = itemView.findViewById(R.id.musical_artist_name)
+        private val directionView: TextView = itemView.findViewById(R.id.duration)
 
         fun bind(trackItem: Track) {
-            Log.d("SPRINT_10", trackItem.trackName)
-            Glide.with(this.itemView.context)
-                .load(trackItem.src)
+            Glide.with(itemView.context)
+                .load(trackItem.artworkUrl100)
                 .placeholder(R.drawable.track_placeholder)
                 .fitCenter()
-                .transform(RoundedCorners(dpToPx(IMG_RADIUS, this.itemView.context)))
+                .transform(RoundedCorners(dpToPx(IMG_RADIUS, itemView.context)))
                 .into(albumPictureView)
 
             trackView.text = trackItem.trackName
-            musicalView.text = trackItem.singerName
-            directionView.text = trackItem.duration
+            musicalView.text = trackItem.artistName
+            directionView.text =
+                SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackItem.trackTimeMillis)
             musicalView.requestLayout()
+
         }
 
         private fun dpToPx(dp: Float, context: Context): Int {
             return TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
                 dp,
-                context.resources.displayMetrics).toInt()
+                context.resources.displayMetrics
+            ).toInt()
         }
 
         companion object {
